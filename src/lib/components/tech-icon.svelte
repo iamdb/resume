92187d7584d4a27f3ad38e5797df3781c2779bff
David Benjamin <db@iamdb.co>
2022-10-21 13:16:54 -0700

    refine language list

--- src/lib/components/tech-icon.svelte
+++ src/lib/components/tech-icon.svelte
@@ -4,9 +4,11 @@
 	export let color: string | undefined = undefined;
 </script>
 
-<div class="flex flex-row items-center">
-	<iconify-icon style:color class="text-4xl" {icon} />
+<div class="flex flex-row justify-start items-center">
+	<div class="aspect-square w-8">
+		<iconify-icon style:color width="100%" {icon} />
+	</div>
 	{#if name}
-		<span class="mx-4 text-2xl font-bold cursor-default">{name}</span>
+		<span class="mx-4 text-2xl leading-none font-bold cursor-default">{name}</span>
 	{/if}
 </div>
