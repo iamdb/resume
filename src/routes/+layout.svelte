dbb2b2c87e7461f8bdf9f65415d1380e78802c38
David Benjamin <db@iamdb.co>
2022-10-25 16:44:13 -0700

    update target links to include noopener

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,10 +12,14 @@
 	<footer class="flex flex-col items-center justify-center pb-12">
 		<p class="text-sm leading-none text-grey-500 text-center">&copy; 2022 David Benjamin</p>
 		<div class="text-lg flex flex-row gap-x-4">
-			<a href="https://github.com/iamdb" target="_blank" rel="noreferrer">
+			<a href="https://github.com/iamdb" target="_blank" rel="noreferrer noopener">
 				<iconify-icon icon="simple-icons:github" />
 			</a>
-			<a href="https://www.linkedin.com/in/davidabenjamin" target="_blank" rel="noreferrer">
+			<a
+				href="https://www.linkedin.com/in/davidabenjamin"
+				target="_blank"
+				rel="noreferrer noopener"
+			>
 				<iconify-icon icon="simple-icons:linkedin" />
 			</a>
 		</div>
