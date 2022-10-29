e7f5b87e7058a6854fdd043d5dbf21a0b5713df0
David Benjamin <db@iamdb.co>
2022-10-29 14:35:01 -0700

    fix so images only load once

--- src/lib/components/concert-photo.svelte
+++ src/lib/components/concert-photo.svelte
@@ -29,25 +29,23 @@
 	class="concert-photo relative bg-grey-800 transition-opacity duration-300 flex flex-col overflow-hidden"
 	style="padding-top: 66.6%">
 	<div class="absolute top-0 left-0 w-full h-full">
-		{#if showImage}
-			<Image alt={src} {src} />
-			{#if meta && (isHovered || disableHover)}
+		<Image hideImage={!showImage} alt={src} {src} />
+		{#if meta && showImage && (isHovered || disableHover)}
+			<div
+				bind:offsetWidth={metaWidth}
+				transition:fly|local={{ x: metaWidth, easing: quintOut }}
+				class="absolute bottom-0 right-0 px-6 py-4 bg-black-900/80 rounded-tl-lg">
+				<h4 class="text-white-200 leading-none mb-4 pr-10">{meta.name}</h4>
 				<div
-					bind:offsetWidth={metaWidth}
-					transition:fly|local={{ x: metaWidth, easing: quintOut }}
-					class="absolute bottom-0 right-0 px-6 py-4 bg-black-900/80 rounded-tl-lg">
-					<h4 class="text-white-200 leading-none mb-4 pr-10">{meta.name}</h4>
-					<div
-						class="flex text-white-400 text-base leading-tight flex-row items-center justify-between gap-x-8">
-						<span>
-							<iconify-icon inline icon="carbon:location-filled" class="mr-2" />{meta.location}
-						</span>
-						<span class="text-sm">
-							<iconify-icon inline icon="clarity:date-line" class="mr-2" />{meta.date}
-						</span>
-					</div>
+					class="flex text-white-400 text-base leading-tight flex-row items-center justify-between gap-x-8">
+					<span>
+						<iconify-icon inline icon="carbon:location-filled" class="mr-2" />{meta.location}
+					</span>
+					<span class="text-sm">
+						<iconify-icon inline icon="clarity:date-line" class="mr-2" />{meta.date}
+					</span>
 				</div>
-			{/if}
+			</div>
 		{/if}
 	</div>
 </div>
