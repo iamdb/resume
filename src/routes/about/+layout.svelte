a48aa63301b2c365679cc4fa926225c0e077c69b
David Benjamin <db@iamdb.co>
2022-10-31 08:43:32 -0700

    move sunset photo to background layout adjustments tech icon fix

--- src/routes/about/+layout.svelte
+++ src/routes/about/+layout.svelte
@@ -1,15 +1,10 @@
 <script type="ts">
-	import sunset from './ca_sunset.jpg';
 	import PageHead from '$lib/components/page-head.svelte';
 </script>
 
 <PageHead title="About Me" />
 
-<div class="fixed top-0 left-0 z-0 h-1/3 w-full overflow-hidden">
-	<img
-		class="object-cover opacity-70 object-bottom w-full h-full"
-		src={sunset}
-		alt="Santa Monica Sunset" />
+<div class="absolute top-0 left-0 z-0 w-full sunset h-1/4">
 	<div
 		class="bg-gradient-to-b absolute top-0 left-0 w-full h-full from-[rgba(0,0,0,.25)] to-black-500" />
 </div>
@@ -18,3 +13,12 @@
 	<h1>About Me <sub class="text-base md:text-lg">(David Benjamin)</sub></h1>
 </div>
 <div class="relative z-10"><slot /></div>
+
+<style>
+	.sunset {
+		background-image: url(/photos/ca_sunset.jpg);
+		background-position: center bottom;
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+</style>
