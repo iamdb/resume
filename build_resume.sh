e7e890cc5d3704fe6c11da8ee026ee2b0b5efee6
David Benjamin <db@iamdb.co>
2023-01-31 15:17:35 -0800

    add downloading icons and outputting

--- build_resume.sh
+++ build_resume.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+npx tsx pandoc/icons/icons.ts
+npx tsx pandoc/github-repos.ts
 curl -s https://wakatime.com/share/@iamdb/9d4f49c3-737c-4cc6-b178-1a3abc5dd2e3.json | yq e -P '.data | .[] | select(.name != "Other" and .name != "conf" and .name != "JSON" and .name != "JSX" and .name != "SCSS" and .name != "INI" and .name != "Liquid") | [.]' | yq -e -P '{"languages": .[0:10]}' > ./pandoc/languages.yaml
 pandoc ./pandoc/resume.md -o ./resume.pdf --from markdown+smart --template ./pandoc/resume.latex --pdf-engine=xelatex --pdf-engine-opt=--shell-escape --metadata-file=./src/lib/content/work-experience.yaml --metadata-file=./pandoc/languages.yaml --metadata-file=./pandoc/github-repos.yaml
