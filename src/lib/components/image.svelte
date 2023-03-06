433b0b05db258e72cc3e9f4156791c0794c1760e
David Benjamin <db@iamdb.co>
2023-03-05 16:52:10 -0800

    try to smooth out image loading

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
 	export let alt: string;
 	export let src: string;
+
+	let img: HTMLImageElement;
+	const loaded = writable(false);
+
+	onMount(() => {
+		img.addEventListener('load', () => {
+			loaded.set(true);
+		});
+	});
 </script>
 
-<div class="relative h-full overflow-hidden">
-	<img decoding="async" loading="lazy" class="object-cover block w-full h-full" {alt} {src} />
+<div style:padding-top="66%" class="relative h-full overflow-hidden">
+	<img
+		bind:this={img}
+		decoding="async"
+		loading="lazy"
+		class:opacity-0={!loaded}
+		class:opacity-100={loaded}
+		class="absolute top-0 left-0 object-cover transition-opacity block w-full h-full"
+		{alt}
+		{src} />
 </div>
