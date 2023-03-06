004e4795414459423ca6a044804d93e64e43eab6
David Benjamin <db@iamdb.co>
2023-03-06 09:54:39 -0800

    update og tags

--- src/lib/components/page-head.svelte
+++ src/lib/components/page-head.svelte
@@ -11,17 +11,19 @@
 		<title>David Benjamin</title>
 		<meta property="og:title" content="David Benjamin" />
 	{/if}
+
 	<meta property="og:url" content={$page?.url.toString()} />
+
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website" />
 	<meta
 		property="og:description"
 		content="I am a software engineer with over a decade of experience building dependable software." />
-	<meta property="og:image" content="" />
+	<meta property="og:image" content="https://www.iamdb.co/photos/ca_sunset_cropped.jpg" />
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta
 		property="twitter:description"
 		content="I am a software engineer with over a decade of experience building dependable software." />
-	<meta property="twitter:image" content="" />
+	<meta property="twitter:image" content="https://www.iamdb.co/photos/ca_sunset_cropped.jpg" />
 </svelte:head>
