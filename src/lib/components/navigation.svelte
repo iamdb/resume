be17d0dc05b67ad2ada809edd557c40b32d42c45
David Benjamin <db@iamdb.co>
2023-10-16 07:46:55 -0700

    add download pdf link for resume

--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -6,11 +6,13 @@
 	let windowWidth = 0;
 
 	const isOpen = writable(false);
+	$: isDesktop = windowWidth >= 768;
 
 	const toggleMenu = () => {
-		if (windowWidth >= 768) {
+		if (isDesktop) {
 			return;
 		}
+
 		if ($isOpen) {
 			isOpen.set(false);
 		} else {
@@ -25,7 +27,7 @@
 	});
 
 	const onWindowResize = () => {
-		if (windowWidth >= 768 && $isOpen) {
+		if ($isOpen) {
 			isOpen.set(false);
 		}
 	};
@@ -36,22 +38,14 @@
 <nav
 	class="flex-col max-h-16 md:flex-row flex sticky top-0 z-10 md:justify-between md:items-center">
 	<svelte:element
-		this={windowWidth >= 768 ? 'a' : 'button'}
-		href={windowWidth >= 768 ? '/' : null}
+		this={isDesktop ? 'a' : 'button'}
+		href={isDesktop ? '/' : null}
 		on:click={toggleMenu}
 		class="uppercase relative aspect-square p-1 z-10 md:mr-2 md:self-stretch transition-colors bg-lightkhaki md:bg-perry text-darkgrey text-center flex items-center justify-center text-2xl font-bold">
 		<span
 			class="bg-khaki leading-none h-full px-3 md:w-full md:h-full border-2 border-darkgrey md:border-powder flex items-center justify-center"
 			>DB</span>
 	</svelte:element>
-	<!-- {#if !$isOpen} -->
-	<!-- {:else} -->
-	<!-- 	<a -->
-	<!-- 		class="uppercase md:mr-2 flex-grow-0 md:self-stretch transition-colors px-3 bg-perry text-darkgrey text-center flex items-center justify-center text-lg font-bold" -->
-	<!-- 		href="/"> -->
-	<!-- 		<span class="bg-khaki leading-none p-2 border-2 border-powder">DB</span> -->
-	<!-- 	</a> -->
-	<!-- {/if} -->
 	<div
 		class:translate-y-full={$isOpen}
 		class="absolute transition-transform flex flex-col md:flex-row md:items-center justify-center z-0 bottom-0 bg-khaki border-b-8 md:border-none border-lightkhaki left-0 w-full md:relative">
--- src/routes/resume/header.svelte
+++ src/routes/resume/header.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import PageTitle from '$lib/components/page-title.svelte';
+	import Icon from '@iconify/svelte';
 	import IconGithub from 'virtual:icons/simple-icons/github';
 	import IconLinkedIn from 'virtual:icons/simple-icons/linkedin';
 </script>
 
-<section>
+<section class="relative">
 	<div class="flex flex-col items-stretch">
 		<PageTitle>David Benjamin</PageTitle>
 		<div class="flex flex-col justify-around items-center md:flex-row lg:gap-x-72 lg:justify-start">
@@ -33,5 +34,12 @@
 			I am an experienced software engineer who loves building applications for humans and computers
 			alike. I can make any small-to-large project a reality or lead a team to make it happen.
 		</p>
+	</div>
+	<div class="absolute top-0 right-0">
+		<a
+			class="font-bold leading-none uppercase text-lg pl-4 hover:bg-perry transition-colors pr-3 py-2 bg-powder border-2 rounded-md border-perry"
+			href="/DavidBenjamin_resume.pdf">
+			Download PDF
+			<Icon height="1.5em" class="inline-block align-middle" icon="mdi:download-circle" /></a>
 	</div>
 </section>
